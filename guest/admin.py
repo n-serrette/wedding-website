03672a95118a8a976b22cc4bb8d55a23cfa4277e
n-serrette <serrette.nicola@gmail.com>
2020-08-20 08:08:01 +0200

    add guest and party model

--- guest/admin.py
+++ guest/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Guest, Party
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "is_child", "party"]
+
+
+class GuestInline(admin.TabularInline):
+    extra = 0
+    model = Guest
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [GuestInline]
--- guest/models.py
+++ guest/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Party(models.Model):
+    """Party model"""
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Guest(models.Model):
+    """Guest model"""
+    party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    is_child = models.BooleanField(default=False)
+    attending_ceremony = models.BooleanField(default=False)
+    attending_cocktail = models.BooleanField(default=False)
+    attending_dinner = models.BooleanField(default=False)
+    attending_brunch = models.BooleanField(default=False)
+    invited_ceremony = models.BooleanField(default=True)
+    invited_cocktail = models.BooleanField(default=True)
+    invited_dinner = models.BooleanField(default=True)
+    invited_brunch = models.BooleanField(default=True)
+    comment = models.TextField(default="")
